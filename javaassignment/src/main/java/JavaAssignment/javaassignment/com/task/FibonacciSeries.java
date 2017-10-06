1a72452ed9171d79318438bc9c66d0a9c19d99b2
rawast <rawast@BLRGMBENCH41245.sapient.com>
2017-10-06 16:39:51 +0530

    Complete

--- javaassignment/src/main/java/JavaAssignment/javaassignment/com/task/FibonacciSeries.java
+++ javaassignment/src/main/java/JavaAssignment/javaassignment/com/task/FibonacciSeries.java
@@ -1,8 +1,30 @@
 package JavaAssignment.javaassignment.com.task;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FibonacciSeries {
 
 	public static void main(String[] args) {
+		System.out.println(getFabonacciSeries(5));
+	}
+	
+	public static List<Integer> getFabonacciSeries(int number) {
+		
+		List<Integer> series = new ArrayList<Integer>();
+		int firstNum = 0;
+		int secondNum = 1;
+		
+		series.add(firstNum);
+		series.add(secondNum);
+		for(int i = 0; i < number ; i++) {
+			int next = firstNum + secondNum;
+			firstNum = secondNum;
+			secondNum = next;
+			series.add(next);
+		}
+		
+		return series;
 	}
 
 }
